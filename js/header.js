a1da01695e6b9ea52ab96af7dd984f9d8527f3ed
Asher Bordelon <frdyttedf1@outlook.com>
2019-01-18 19:12:09 -0600

    eEEEEEEEEEEEEEEE

---
+++ js/header.js
@@ -0,0 +1,34 @@
+var header = (function() {
+
+  var _bind = function() {
+    window.addEventListener("resize", function(event) {
+      render();
+    }, false);
+    helper.eA(".container").forEach(function(arrayItem, index) {
+      arrayItem.addEventListener("transitionend", function() {
+        render();
+      }, false);
+    });
+  };
+
+  var render = function() {
+    var html = helper.e("html");
+    var header = helper.e(".header");
+    var link = helper.e(".link");
+    var height = parseInt(getComputedStyle(header).height, 10);
+    var fontSize = parseInt(getComputedStyle(html).fontSize, 10);
+    link.style.marginTop = (height + fontSize) + "px";
+  };
+
+  var init = function() {
+    _bind();
+    render();
+  };
+
+  // exposed methods
+  return {
+    init: init,
+    render: render
+  };
+
+})();
---
+++ js/update.js
@@ -0,0 +1,78 @@
+var update = (function() {
+
+  var _update_100 = function(data) {
+    var time = new Date().getTime();
+    data.bookmarks.forEach(function(arrayItem, index) {
+      time = time + 1;
+      arrayItem.timeStamp = time;
+    });
+    data.version = 1.00;
+    return data;
+  };
+
+  var _update_200 = function(data) {
+    state.change({
+      path: "layout.theme",
+      value: data.theme
+    });
+    data = {
+      state: state.get(),
+      bookmarks: data.bookmarks
+    };
+    data.version = 2.00;
+    return data;
+  };
+
+  var _update_210 = function(data) {
+    data.state.layout.theme = {
+      current: data.state.layout.theme,
+      random: false
+    };
+    data.version = 2.10;
+    return data;
+  };
+
+  var _update_230 = function(data) {
+    data.state.layout.theme.random = {
+      active: data.state.layout.theme.random,
+      style: "any"
+    };
+    data.version = 2.30;
+    return data;
+  };
+
+  // var _update_300 = function(data) {
+  //   data.version = 3.00;
+  //   return data;
+  // };
+
+  function render(data) {
+    if (!("version" in data)) {
+      console.log("\trunning update", 1.00);
+      data = _update_100(data);
+    };
+    if (data.version < 2.00) {
+      console.log("\trunning update", 2.00);
+      data = _update_200(data);
+    };
+    if (data.version < 2.10) {
+      console.log("\trunning update", 2.10);
+      data = _update_210(data);
+    };
+    if (data.version < 2.30) {
+      console.log("\trunning update", 2.30);
+      data = _update_230(data);
+    };
+    // if (data.version < 3.00) {
+    //   console.log("\t# running update", 3.00);
+    //   data = _update_300(data);
+    // };
+    return data;
+  };
+
+  // exposed methods
+  return {
+    render: render
+  };
+
+})();
